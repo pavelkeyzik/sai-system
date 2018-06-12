88e2d89524ec315263b39d032587e2b56fe9b09e
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-06-12 11:21:30 +0300

    Added capability of working with Environment Variables

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const PORT = process.env.PORT || 8080
 
+if(process.env.NODE_ENV !== 'prod') {
+  require('dotenv').load();
+}
+
 const server = http.createServer((req, res) => {
   res.end(`I'm still working... Are you happy?`);
 });
